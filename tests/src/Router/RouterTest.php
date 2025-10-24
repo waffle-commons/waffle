01e1159a352935e511864ea6b2150fe35246f6b5
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-10-24 10:53:16 +0200

    Fully tested `Router`

--- tests/src/Router/RouterTest.php
+++ tests/src/Router/RouterTest.php
@@ -238,6 +238,30 @@ final class RouterTest extends TestCase
     }
 
     /**
+     * This test validates the production optimization feature of route caching.
+     */
+    public function testBootLoadsRoutesFromCacheInProduction(): void
+    {
+        putenv('APP_ENV=prod');
+        $cacheFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'waffle_routes_cache.php';
+        $routes = $this->provideRoutesArray();
+        $content = '<?php return ' . var_export($routes, true) . ';';
+        file_put_contents($cacheFile, $content, LOCK_EX);
+
+        $container = $this->createRealContainer(level: 2);
+        $this->router->boot(container: $container);
+        static::assertFileExists($cacheFile, 'The router should have created a cache file.');
+
+        $cachedRoutes = $this->router->routes;
+        static::assertSame($routes, $cachedRoutes, 'The cache file should have the same routes.');
+        static::assertNotEmpty($cachedRoutes, 'The cache file should not be empty.');
+        static::assertCount(5, $cachedRoutes, 'The cache file should contain the correct number of routes.');
+
+        unlink($cacheFile);
+        putenv('APP_ENV=test');
+    }
+
+    /**
      * This test validates that the router is robust and does not crash when provided with
      * a non-existent controller directory. A well-behaved framework should handle this
      * configuration error gracefully instead of throwing a fatal error.
@@ -261,5 +285,51 @@ final class RouterTest extends TestCase
             $badRouter->boot(container: $container),
             'The boot method should still return the router instance.',
         );
+    }
+
+    private function provideRoutesArray(): array
+    {
+        return [
+            [
+                'classname' => 'WaffleTests\Helper\Controller\TempController',
+                'method' => 'list',
+                'arguments' => [],
+                'path' => '/users',
+                'name' => 'user_users_list',
+            ],
+            [
+                'classname' => 'WaffleTests\Helper\Controller\TempController',
+                'method' => 'show',
+                'arguments' => [
+                    'id' => 'int',
+                ],
+                'path' => '/users/{id}',
+                'name' => 'user_users_show',
+            ],
+            [
+                'classname' => 'WaffleTests\Helper\Controller\TempController',
+                'method' => 'details',
+                'arguments' => [
+                    'id' => 'int',
+                    'slug' => 'string',
+                ],
+                'path' => '/users/{id}/{slug}',
+                'name' => 'user_users_details',
+            ],
+            [
+                'classname' => 'WaffleTests\Helper\Controller\TempController',
+                'method' => 'profile',
+                'arguments' => [],
+                'path' => '/users/profile/view',
+                'name' => 'user_users_profile_view',
+            ],
+            [
+                'classname' => 'WaffleTests\Helper\Controller\TempController',
+                'method' => 'throwError',
+                'arguments' => [],
+                'path' => '/trigger-error',
+                'name' => 'user_trigger_error',
+            ],
+        ];
     }
 }
